8b0fbffa6cdb38dc5ce9fed9568379442d7d061f
realmizzer <diamondAlma32@gmail.com>
2025-01-24 16:11:36 +0700

    forgot about this guys

---
+++ src/app/providers/StoreProvider/index.ts
@@ -0,0 +1,2 @@
+export {RootStore} from '~/app/providers/StoreProvider/config/store.ts';
+export {StoreProvider} from '~/app/providers/StoreProvider/ui/StoreProvider.tsx';
---
+++ src/app/providers/index.ts
@@ -0,0 +1,1 @@
+export {RootStore, StoreProvider} from './StoreProvider';
