486fe48e343267e834aa763bf5d97b27d1a0048f
realmizzer <diamondAlma32@gmail.com>
2025-01-24 15:52:54 +0700

    fix: babel and aliases

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [['@babel/plugin-proposal-class-properties']],
+  plugins: [
+    ['@babel/plugin-proposal-class-properties', {loose: true}],
+    [
+      'module-resolver',
+      {
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+        root: ['.'],
+        alias: {
+          '~': './src',
+        },
+      },
+    ],
+  ],
   assumptions: {
     setPublicClassFields: false,
   },
