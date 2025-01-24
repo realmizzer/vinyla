ef9f5ef2db176819e317960c21153b0f24355afe
realmizzer <diamondAlma32@gmail.com>
2025-01-24 14:25:26 +0700

    add: new npm dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['@react-native', '@feature-sliced'],
+  extends: ['@feature-sliced'],
   parser: '@typescript-eslint/parser',
   settings: {
     'import/resolver': {
